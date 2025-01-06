9a99b1668d1ba4e7c72faf333a6a935645387c9f
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2025-01-06 23:23:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState , useEffect } from "react";
+import Show from "./component/Show";
 
 function App() {
   return (
     <div className="App">
-      
+        <Show/>
     </div>
   );
 }
---
+++ src/component/Show.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+const BASE_URL = process.env.REACT_APP_API_URL;
+console.log("API", BASE_URL);
+const Show = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let url = `${BASE_URL}/user/all`;
+                const response = await fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                });
+                const result = await response.json();
+                console.log("result is:", result);
+                if (response.ok) {
+                    setData(result.all || []);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>User List</h1>
+            {data.map((fdata, ind) => (
+                <div key={ind}>
+                    <p>Name: {fdata.name}</p>
+                    <p>Email: {fdata.email}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Show;
